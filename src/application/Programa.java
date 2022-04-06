aaae00db8e9b2addc22c7c5544f61e9175ed652c
William Robert da Veiga Alves <wra.profissional@gmail.com>
2022-04-05 21:59:34 -0300

    Projeto criado

---
+++ src/application/Programa.java
@@ -0,0 +1,15 @@
+package application;
+
+import tabuleiro.Posicao;
+
+public class Programa {
+
+	public static void main(String[] args) {
+	
+		Posicao pos = new Posicao(5, 3);
+		System.out.println(pos);
+
+				
+	}
+
+}
